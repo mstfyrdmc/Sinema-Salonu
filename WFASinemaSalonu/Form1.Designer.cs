63b3132e9190c2794612e4fd7ee144caaa1a4597
mstfyrdmc <45285832+AdamGiby@users.noreply.github.com>
2020-02-15 19:16:28 +0300

    SinemaSalonu

---
+++ WFASinemaSalonu/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace WFASinemaSalonu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnYap = new System.Windows.Forms.Button();
+            this.flpSalon = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // btnYap
+            // 
+            this.btnYap.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnYap.Location = new System.Drawing.Point(36, 18);
+            this.btnYap.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnYap.Name = "btnYap";
+            this.btnYap.Size = new System.Drawing.Size(1556, 92);
+            this.btnYap.TabIndex = 0;
+            this.btnYap.Text = "Sinema Otomasyonu";
+            this.btnYap.UseVisualStyleBackColor = true;
+            this.btnYap.Click += new System.EventHandler(this.btnYap_Click);
+            // 
+            // flpSalon
+            // 
+            this.flpSalon.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flpSalon.Location = new System.Drawing.Point(36, 151);
+            this.flpSalon.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.flpSalon.Name = "flpSalon";
+            this.flpSalon.Size = new System.Drawing.Size(1799, 922);
+            this.flpSalon.TabIndex = 1;
+            this.flpSalon.Paint += new System.Windows.Forms.PaintEventHandler(this.flpSalon_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1476, 972);
+            this.Controls.Add(this.flpSalon);
+            this.Controls.Add(this.btnYap);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnYap;
+        private System.Windows.Forms.FlowLayoutPanel flpSalon;
+    }
+}
+
---
+++ WFASinemaSalonu/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFASinemaSalonu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnYap_Click(object sender, EventArgs e)
+        {
+
+            for (int x = 0; x < 6; x++) // yukarıdan aşağıya sıralanması
+            {
+
+                for (int y = 0; y < 13; y++) // yan yana sıralanması
+                {
+
+                    //koltuk resminin oluşturulması ve standart özellikleri
+                    PictureBox pbKoltuk = new PictureBox();
+                    pbKoltuk.Image = Properties.Resources.koltuk;
+                    pbKoltuk.SizeMode = PictureBoxSizeMode.Zoom;
+                    pbKoltuk.Width = pbKoltuk.Height = 70;
+                    pbKoltuk.BackColor = Color.Indigo;
+                    pbKoltuk.Margin = new Padding(5, 5, 20, 5);                  
+
+                    // arada koridor olmayan koltukların yaklaştırılması
+                    if (y == 0)
+                    {
+                        pbKoltuk.Margin = new Padding(20, 5, 20, 5);
+                    }
+                    else if (y == 1 || y == 3 || y == 8 || y == 10)
+                    {
+                        pbKoltuk.Margin = new Padding(5, 5, 5, 5);
+                    }
+
+                    switch (x) //sıra başı ve sonunda koltuk olmayan yerler için
+                    {
+                        case 0:
+                            if (y == 0 || y == 1 || y == 11 || y == 12)
+                            {
+                                pbKoltuk.BackColor = Color.Transparent;
+                                pbKoltuk.Image = null;                               
+                            }
+                            break;
+
+                        case 2:
+                            if (y == 0 || y == 1 || y == 11 || y == 12)
+                            {
+                                pbKoltuk.BackColor = Color.Transparent;
+                                pbKoltuk.Image = null;
+                            }
+                            break;
+                            
+                        case 4:
+                            if (y == 0 || y == 1 || y == 11 || y == 12)
+                            {
+                                pbKoltuk.BackColor = Color.Transparent;
+                                pbKoltuk.Image = null;
+                            }
+                            break;
+                            
+                    }
+
+                    flpSalon.Controls.Add(pbKoltuk);
+                }
+
+
+
+            }
+
+        }
+
+        private void flpSalon_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
